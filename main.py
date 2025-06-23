c864fa81bb709a58e5dd15a161a0e677eb901720
Jeomon <jeogeoalukka@gmail.com>
2025-06-24 00:21:18 +0530

    main.py updated

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm=ChatGoogleGenerativeAI(model='gemini-2.0-flash')
-agent = Agent(llm=llm,use_vision=True)
 query=input("Enter your query: ")
+agent = Agent(llm=llm,use_vision=True)
 agent.print_response(query)
